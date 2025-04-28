6640e3a987e0f331a6d10c805859e3ab0cb3b292
João Roberto <gomes.junior434@gmail.com>
2025-04-28 01:00:08 -0300

    rebase

--- DotNetInterview/DotNetInterview.Api/obj/Debug/net8.0/DotNetInterview.Api.AssemblyInfo.cs
+++ DotNetInterview/DotNetInterview.Api/obj/Debug/net8.0/DotNetInterview.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetInterview.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec9d2e759dd410129b63a9807641201bd959856")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetInterview.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetInterview.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DotNetInterview/Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ DotNetInterview/Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec9d2e759dd410129b63a9807641201bd959856")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
